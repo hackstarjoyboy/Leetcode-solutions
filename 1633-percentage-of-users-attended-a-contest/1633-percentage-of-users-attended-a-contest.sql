d814a365c9248abf9c2feb3cde7e2813ede7b60d
pranayborkar <143306287+hackstarjoyboy@users.noreply.github.com>
2026-06-10 19:36:23 +0530

    Time: 751 ms (39.16%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,9 @@
+/* Write your PL/SQL query statement below */
+select contest_id ,
+round(count(user_id)*100.0/(select count(*) from users),2) as percentage
+from register
+group by 
+contest_id
+order by
+percentage desc,
+contest_id asc;

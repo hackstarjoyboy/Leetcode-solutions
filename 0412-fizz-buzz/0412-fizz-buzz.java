0d7729b9bba408a3e2f9b3d6beb94d42a850ffa6
pranayborkar <143306287+hackstarjoyboy@users.noreply.github.com>
2026-07-20 19:50:16 +0530

    Time: 2 ms (14.94%), Space: 46.7 MB (83.69%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -2,7 +2,7 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> str=new ArrayList<>();
         for(int i=1;i<=n;i++){
-            if(i%3==0 & i%5==0){
+            if(i%3==0 && i%5==0){
                 str.add("FizzBuzz");
             }else if(i%3==0){
                 str.add("Fizz");

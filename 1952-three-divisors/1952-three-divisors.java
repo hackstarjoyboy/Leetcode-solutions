61c6b2d67561b8a4d1236b252f100ef3696c218e
pranayborkar <143306287+hackstarjoyboy@users.noreply.github.com>
2026-08-14 11:23:28 +0530

    Time: 0 ms (100%), Space: 42.4 MB (6.45%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.java
+++ 1952-three-divisors/1952-three-divisors.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean isThree(int n) {
-      int count=0;
-      for(int i=1;i<=n;i++){
-        if(n%i==0){
-            count++;
-        }
-        if(count>3){
+        if(n<4){
             return false;
         }
-      }  
-      return count==3;
+        int root=(int)Math.sqrt(n);
+        if(root * root!=n){
+            return false;
+        }
+        for(int i=2;i*i<=root;i++){
+            if(root%i==0){
+                return false;
+            }
+        }
+        return true;
     }
 }

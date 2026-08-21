f1c05975ef001f5ad8d834908c47098443e6e62d
pranayborkar <143306287+hackstarjoyboy@users.noreply.github.com>
2026-08-21 18:31:09 +0530

    Time: 10 ms (72.52%), Space: 47.3 MB (84.73%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,8 +1,8 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
         int n=score.length;
-        String[] result =new String[n]; 
         int[][] scorePairs=new int[n][2];
+        String[] result=new String[n];
         for(int i=0;i<n;i++){
             scorePairs[i][0]=score[i];
             scorePairs[i][1]=i;
@@ -12,18 +12,15 @@ class Solution {
             int originalIndex=scorePairs[i][1];
             if(i==0){
                 result[originalIndex]="Gold Medal";
-
             }else if(i==1){
                 result[originalIndex]="Silver Medal";
+
             }else if(i==2){
                 result[originalIndex]="Bronze Medal";
             }else{
                 result[originalIndex]=String.valueOf(i+1);
             }
         }
-       
-       return result;
-
-
+     return result;
     }
 }

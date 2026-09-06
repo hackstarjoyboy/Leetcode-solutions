df6a7d50fb4c9b14b95aa659728d83258266afc8
pranayborkar <143306287+hackstarjoyboy@users.noreply.github.com>
2026-09-06 20:28:55 +0530

    Time: 8 ms (92.21%), Space: 47.3 MB (11.1%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,20 +1,28 @@
 class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
         Arrays.sort(nums);
-       int[] answer=new int[queries.length];
-       for(int i=0;i<queries.length;i++){
-        int currentSum=0;
-        int count=0;
-        for(int j=0;j<nums.length;j++){
-      if(currentSum+nums[j]<=queries[i]){
-        currentSum+=nums[j];
-        count++;
-      }else{
-        break;
-      }
-        }
-      answer[i]=count;
-         }
-        return answer;
-       }
+        int[] answer = new int[queries.length];
+        for (int i = 1; i < nums.length; i++) {
+           nums[i]+=nums[i-1];
     }
+ for(int i=0;i<queries.length;i++){
+    answer[i]=binarySearch(nums,queries[i]);
+ }
+return answer;
+}
+private int binarySearch(int[] prefixSum,int target){
+    int left=0;
+    int right=prefixSum.length-1;
+    int maxsize=0;
+while(left<=right){
+    int mid=left+(right-left)/2;
+    if(prefixSum[mid]<=target){
+        maxsize=mid+1;
+        left=mid+1;
+    }else{
+        right=mid-1;
+    }
+}
+return maxsize;
+}
+}
